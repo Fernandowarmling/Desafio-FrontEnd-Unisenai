e29db63bb6983aa52482181f70369cf3f9ba614d
fernandowarmling <warmlingfernando@gmail.com>
2024-03-13 21:06:02 -0300

    Finalização de projeto

--- contador.js
+++ contador.js
@@ -1,28 +1,63 @@
 console.clear()
 
+
+const cordefundo = document.getElementById("body")
+var cor = "#191970"
+cordefundo.style.backgroundColor = cor
+
+
 function progressivo() {
+
     console.log("Start Função");
     let inicial = 0
-    let limite = 1000
-    let incremental = 1
+    let valorAlvo = 8250000
+    let tempo = 2 * 1000
+    let incremental = 100
+    let limite = valorAlvo / incremental
+
+
+
     document.getElementById("progressivo").innerHTML = inicial;
-    setTimeout(function () {
-        for (var i = 0; i < limite; i++) {
-            inicial = inicial + 1
-            console.log(inicial)
+
+
+    for (var i = 0; i < limite; i++) {
+        /*console.log(i)*/
+        setTimeout(function () {
+            inicial = inicial + incremental
+            /*console.log(inicial)*/
+            /*console.log("limite " + limite)*/
             document.getElementById("progressivo").innerHTML = inicial;
-        }
-    }, 1000);
-
-    
-    
-
-
-
+        }, tempo)
+    }
+    ;
 }
 
 
 
 
-progressivo()
+function trocaback() {
+
+
+
+    console.log("Entou na função ")
+    if (cor === "#191970") {
+
+        cordefundo.style.transition = "background-color 1s";
+
+        cor = "#000000"
+
+        cordefundo.style.backgroundColor = cor;
+
+    } else {
+        cordefundo.style.transition = "background-color 1s";
+
+        cor = "#191970"
+
+        cordefundo.style.backgroundColor = cor;
+    }
+
+
+}
+
+
 
